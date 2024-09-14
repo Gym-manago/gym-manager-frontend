f5d2a578a0b2af557a4f81dcd554e69c4e04e6fd
soyo-kaze <07sameersaini@gmail.com>
2024-09-14 21:43:22 +0530

    Add sidenavbar with routing and tab highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from "/logo.jpeg";
+import style from "./styles/App.module.scss";
+import { SideNav } from "./components";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className={style.main_container}>
+        <SideNav />
+        <section className={style.main_content}>
+          <Routes>
+            <Route index element={<Navigate to="/dashboard" />} />
+            <Route
+              path="/dashboard"
+              element={
+                <>
+                  <img src={logo} className={style.img} />
+                  <h1>Hello Ma!!</h1>
+                </>
+              }
+            />
+            <Route
+              path="/members"
+              element={
+                <>
+                  <img src={logo} className={style.img} />
+                  <h1>Hello Pa!!</h1>
+                </>
+              }
+            />
+          </Routes>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
